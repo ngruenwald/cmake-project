cdb9ab5813ae613ebda5836d9fdb2ea371cd62c5
ngruenwald <40895194+ngruenwald@users.noreply.github.com>
2023-10-28 12:34:17 +0200

    maint: add openssl to test project

--- example/main.cpp
+++ example/main.cpp
@@ -1,6 +1,13 @@
 #include <fmt/core.h>
 
+#include <openssl/evp.h>
+#include <openssl/bio.h>
+#include <openssl/err.h>
+
+
 int main(int argc, char** argv)
 {
     fmt::print("hello world\n");
+
+    auto ctx = EVP_MD_CTX_new();
 }
